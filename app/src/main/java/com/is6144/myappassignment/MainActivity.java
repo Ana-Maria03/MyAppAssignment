6a8d329596a7ef39b3a037a1f01139c1cbb6bb5d
Ana-Maria03 <85620955+Ana-Maria03@users.noreply.github.com>
2022-11-26 15:58:13 +0000

    Second Commit

--- app/src/main/java/com/is6144/myappassignment/MainActivity.java
+++ app/src/main/java/com/is6144/myappassignment/MainActivity.java
@@ -1,11 +1,11 @@
 package com.is6144.myappassignment;
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,17 +14,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     Button btn1;
-
+    Button btn_scan;
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        btn1= findViewById(R.id.btn1);
+        btn1 = findViewById(R.id.btn1);
 
+        btn_scan = findViewById (R.id.btn_scan);
+
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a=new Intent(MainActivity.this, ScannerClass.class);
+                startActivity(a);
+            }
+        });
 
         btn1.setOnClickListener(new View.OnClickListener(){
 
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
 
 
--- app/src/main/java/com/is6144/myappassignment/SecondActivity.java
+++ app/src/main/java/com/is6144/myappassignment/SecondActivity.java
@@ -15,10 +15,15 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.review);
 
 
-        Book book1 = new Book("The Shining", 1);
-        Book book2 = new Book("Animal Farm", 2);
-        Book book3 = new Book("Jane Eyre", 3);
-        Book[] arrayBook={book1, book2, book3};
+        Book book1 = new Book("The Shining by Stephen King", 1);
+        Book book2 = new Book("Animal Farm by George Orwell", 2);
+        Book book3 = new Book("Jane Eyre by Charlotte Bronte", 3);
+        Book book4 = new Book("Communist Manifest by Marx and Engels", 4);
+        Book book5 = new Book("The Wonderful Wizard of Oz by L. Frank Baum", 5);
+        Book book6 = new Book("Alice in Wonderland by Lewis Carroll", 6);
+        Book book7 = new Book("Just Kids by Patti Smith", 7);
+        Book book8 = new Book("Moby Dick by Herman Melville", 8);
+        Book[] arrayBook={book1, book2, book3,book4,book5,book6,book7,book8};
 
         RecyclerView recyclerView = findViewById(R.id.rView);
         BookViewHolderAdapter adapter= new BookViewHolderAdapter(arrayBook);
